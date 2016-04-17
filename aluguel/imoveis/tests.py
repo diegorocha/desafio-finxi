bb4ea908c7b67d9f6676d7d9750968cd51fd88ae
Diego Rocha <diego@diegorocha.com.br>
2016-04-16 21:23:52 -0300

    Incluido primeiro teste de view

--- aluguel/imoveis/tests.py
+++ aluguel/imoveis/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HomeViewTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+        
+    def test_get(self):
+        self.assertTrue(200, self.resp.status_code)
